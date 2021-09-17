aa0b959e09a90bdb61c5b0c78b312a3312cb598c
prajwalnandeshwar <prajwaljn1008@gmail.com>
2021-09-17 12:03:20 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+let button = document.getElementById("theboxes");
+button.addEventListener("click",myfunction);
+
+var oldvalue = 0;
+var i =1;
+function myfunction(){
+    var n = document.getElementById("number").value;
+    n = parseInt(oldvalue)+parseInt(n);
+        for(;i<=n;i++){
+            var box = document.createElement('div')
+            box.classList.add('myDiv')
+            document.getElementById('box').appendChild(box)
+            box.innerHTML =i;
+        oldvalue = document.getElementById('box').lastElementChild.innerHTML;
+}
+}
